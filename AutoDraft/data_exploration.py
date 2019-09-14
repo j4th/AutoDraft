ba590e771e630a7265e23e19b4dd6381e657de5e
j4th <justinforth@gmail.com>
2019-09-14 15:36:10 -0700

    added ability to get roster of all players that played for a team across a range of seasons

--- AutoDraft/data_exploration.py
+++ AutoDraft/data_exploration.py
@@ -74,6 +74,17 @@ roster_year = st.text_input('Enter a season ID (eg. 20182019 for the 2018-2019 s
 st.write("Here is that team's roster for that year:")
 st.dataframe(get_roster(team_id=int(roster_team), season_id=int(roster_year)))
 
+st.write("Now we'll get a roster that covers all players that were active in a range of season. NOTE: this uses the team entered earlier.")
 
+@st.cache
+def merge_rosters(team_id=22, season_id_list=[20152016, 20162017, 20172018, 20182019]):
+    merged_roster_df = pd.DataFrame()
+    for season in season_id_list:
+        roster_df = get_roster(team_id=team_id, season_id=season)
+        merged_roster_df = merged_roster_df.append(roster_df)
+    merged_roster_df.drop_duplicates(inplace=True)
+    return merged_roster_df
 
+merged_roster = merge_rosters(team_id=roster_team)
+st.dataframe(merged_roster)
 
