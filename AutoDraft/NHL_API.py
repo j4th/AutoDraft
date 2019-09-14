f6e14ded5be92089964551e67f581d2fe55f9a0f
j4th <justinforth@gmail.com>
2019-09-14 14:08:16 -0700

    moved unnecessary functions into NHL_API (currently non-functional)

---
+++ AutoDraft/NHL_API.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np 
+import pandas as pd
+from pandas.io.json import json_normalize
+import requests as rqsts  
+
+def generate_games_df(schedule_df):
+    games_df = pd.DataFrame()
+    games_series = schedule_df.games
+    st.dataframe(games_series)
+    for day in games_series:
+        for game in day:
+            if game['gameType'] == 'R': 
+                st.dataframe(game)
+                game_id = game['gamePk']
+                game_teams = game['teams']
+                away_team = game_teams['away']
+                home_team = game_teams['home']
+                st.write(home_team.keys())
+                break
+                # games_df.append(game, ignore_index=True)
+    st.dataframe(games_df)
+    return
+
+@st.cache
+def get_season_schedule(seasons_df=get_seasons(), season_id=20182019):
+    season_series = seasons_df.loc['{}'.format(season_id), :]
+    st.table(season_series)
+    season_start = season_series['regularSeasonStartDate']
+    season_end = season_series['regularSeasonEndDate']
+    st.text('Season ID: {0}\nSeason start: {1}\nSeason end: {2}'.format(season_id, season_start, season_end))
+    schedule_response = rqsts.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate={0}&endDate={1}'.format(season_start, season_end))
+    if schedule_response.status_code != 200:
+        st.error('Failed attempt to get full season schedule.')
+        return
+    schedule = schedule_response.content
+    schedule_df = pd.read_json(schedule)
+    schedule_df = json_normalize(schedule_df.dates)
+    schedule_df.set_index('date', inplace=True)
+    # schedule_df = json_normalize(schedule_df.games)
+    # st.dataframe(schedule_df.games)
+    generate_games_df(schedule_df)
+    return schedule_df
+
+st.dataframe(get_season_schedule(seasons_df=seasons_df))
--- AutoDraft/data_exploration.py
+++ AutoDraft/data_exploration.py
@@ -25,6 +25,24 @@ st.write('Here is the full dataframe of NHL seasons with available stats:')
 seasons_df = get_seasons()
 st.dataframe(seasons_df)
 
+def generate_games_df(schedule_df):
+    games_df = pd.DataFrame()
+    games_series = schedule_df.games
+    st.dataframe(games_series)
+    for day in games_series:
+        for game in day:
+            if game['gameType'] == 'R': 
+                st.dataframe(game)
+                game_id = game['gamePk']
+                game_teams = game['teams']
+                away_team = game_teams['away']
+                home_team = game_teams['home']
+                st.write(home_team.keys())
+                break
+                # games_df.append(game, ignore_index=True)
+    st.dataframe(games_df)
+    return
+
 @st.cache
 def get_season_schedule(seasons_df=get_seasons(), season_id=20182019):
     season_series = seasons_df.loc['{}'.format(season_id), :]
@@ -38,7 +56,11 @@ def get_season_schedule(seasons_df=get_seasons(), season_id=20182019):
         return
     schedule = schedule_response.content
     schedule_df = pd.read_json(schedule)
-    st.dataframe(schedule_df)
+    schedule_df = json_normalize(schedule_df.dates)
+    schedule_df.set_index('date', inplace=True)
+    # schedule_df = json_normalize(schedule_df.games)
+    # st.dataframe(schedule_df.games)
+    generate_games_df(schedule_df)
     return schedule_df
 
 st.dataframe(get_season_schedule(seasons_df=seasons_df))
