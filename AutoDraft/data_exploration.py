2b8a848a32261de89b1841af7b0afec3f4e46d66
j4th <justinforth@gmail.com>
2019-09-14 21:26:08 -0700

    created function to get all of a players games in a season into a df. NOTE: there are stil nan issues

--- AutoDraft/data_exploration.py
+++ AutoDraft/data_exploration.py
@@ -89,20 +89,50 @@ merged_roster = merge_team_rosters(team_id=roster_team)
 st.dataframe(merged_roster)
 
 st.write("We'll start collecting time-series sets now for exploration.")
+roster_player = st.text_input('Enter a player ID from the table above:', '8477934')
 
 @st.cache
 def get_player_season_game_stats(player_id=8478402, season_id=20182019):
-    stats_response = rqsts.get('https://statsapi.web.nhl.com/api/v1/people/{0}?stats=gameLog&season={1}'.format(player_id, season_id))
+    stats_response = rqsts.get('https://statsapi.web.nhl.com/api/v1/people/{0}/stats?stats=gameLog&season={1}'.format(player_id, season_id))
     if stats_response.status_code != 200:
         st.error("Failed attempt to get player {0}'s game stats for season {1}.".format(player_id, season_id))
         return
     stats = stats_response.content
     stats_df = pd.read_json(stats)
-    st.dataframe(stats_df)
-    stats_df = stats_df.people
-    stats_dict = dict(stats_df)
-    st.write(stats_dict)
-    return
+    # st.dataframe(stats_df)
+    stats_df = json_normalize(stats_df.stats)
+    # st.dataframe(stats_df)
+    stats_df = stats_df.splits
+    stats_array = stats_df.array
+    # st.dataframe(stats_array)
+    stats_list = stats_array[0]
+    stats_df = pd.DataFrame()
+    for game in stats_list:
+        game_df = pd.DataFrame.from_dict(game).transpose()
+        clean_df = pd.DataFrame()
+        # st.dataframe(game_df)
+        for stat_type, stat_series in game_df.iterrows():
+            try:
+                stat_series.drop_duplicates(inplace=True)
+            except SystemError:
+                pass
+            stat_series.dropna(inplace=True)
+            stat_df = pd.DataFrame(stat_series).transpose()
+            new_columns = [stat_type + column.capitalize() for column in stat_df.columns if len(stat_df.columns) != 1]
+            if len(new_columns) == 0: new_columns = stat_df.index
+            stat_df.reset_index(drop=True, inplace=True)
+            stat_df.columns = new_columns
+            # st.write(new_columns)
+            # st.dataframe(stat_df)
+            clean_df = pd.concat([clean_df, stat_df], axis=1)
+            # st.dataframe(clean_df)
+        game_df = clean_df.drop('gameContent', axis=1)
+        game_df.set_index('gameGamepk', inplace=True)
+        # st.dataframe(game_df)
+        # st.dataframe(clean_df)
+        stats_df = stats_df.append(game_df)
+        # st.dataframe(stats_df)
+    st.dataframe(stats_df) # 2018021268
+    return stats_df
 
-get_player_season_game_stats()
-
+player_df = get_player_season_game_stats(player_id=roster_player)
