75b82200693ba97416592db9abf4ad5f7fc3ddc1
Ubuntu <ubuntu@ip-172-31-60-114.us-west-2.compute.internal>
2019-09-18 04:32:44 +0000

    little more ARIMA

--- AutoDraft/arima.py
+++ AutoDraft/arima.py
@@ -5,7 +5,7 @@ from pandas.plotting import autocorrelation_plot
 from statsmodels.tsa.arima_model import ARIMA
 
 @st.cache
-def load_csv(path='../data/oilers_multi_season_cumulative_points.csv'):
+def load_csv(path='./data/full_dataset_4_seasons.csv'):
     data = pd.read_csv(path)
     return data
 
@@ -25,7 +25,16 @@ autocorrelation_plot(smaller_test_player)
 fig = plt.gcf()
 st.pyplot(fig)
 
-model = ARIMA(smaller_test_player, order=(5,1,0))
+model = ARIMA(smaller_test_player, order=(3,1,1))
 model_fit = model.fit(disp=0)
-st.write(model_fit.summary())
+st.text(model_fit.summary())
 
+residuals = pd.DataFrame(model_fit.resid)
+residuals.plot()
+fig = plt.gcf()
+st.pyplot(fig)
+
+residuals.plot(kind='kde')
+fig = plt.gcf()
+st.pyplot(fig)
+st.write(residuals.describe())
