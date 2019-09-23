53f365e8572283a38bcf44d685919cd18013ff16
Ubuntu <ubuntu@ip-172-31-3-240.us-west-2.compute.internal>
2019-09-23 02:50:02 +0000

    removed redundant functions now that they are packaged

--- AutoDraft/arima.py
+++ AutoDraft/arima.py
@@ -56,39 +56,6 @@ st.dataframe(smaller_test_player)
 # fig = plt.gcf()
 # st.pyplot(fig)
 
-@st.cache
-def calculate_errors(residuals):
-    num_residuals = len(residuals)
-    mfe = (residuals.sum() / num_residuals).tolist()[0]
-    mae = (residuals.abs().sum() / num_residuals).tolist()[0]
-    rmse = (residuals.pow(2).sum().pow(0.5)).tolist()[0]
-    residuals = residuals.values
-    residuals = [value.item() for value in residuals]
-    return mfe, mae, rmse, residuals
-
-@st.cache
-def unpack_coeffs(coeff1=None, coeff2=None, coeff3=None):
-    # if coeff3:
-    return coeff1, coeff2, coeff3
-    # return coeff1, coeff2
-
-@st.cache
-def get_predictions(data, model_fit):
-    predictions = model_fit.predict('2018-10-03', '2019-04-06', typ='levels')
-    real = data[data['date'] >= '2018-10-03'].cumStatpoints.to_numpy()
-    st.dataframe(predictions)
-    st.dataframe(real)
-    return
-
-@st.cache
-def calculate_test_residuals(prediction_array, test_data):
-    prediction_array = prediction_array.reshape(len(test_data), 1)
-    test_data = test_data.values
-    residuals = np.subtract(test_data, prediction_array)
-    residuals = residuals.tolist()
-    residuals = pd.DataFrame(residuals)
-    return residuals
-
 # train_residuals.plot()
 # fig = plt.gcf()
 # st.pyplot(fig)
@@ -105,121 +72,9 @@ def calculate_test_residuals(prediction_array, test_data):
 # fig = plt.gcf()
 # st.pyplot(fig)
 
-# @st.cache
-def player_arima(data, player_name, index='date' ,feature='cumStatpoints' , forecast_from='2018-10-03', transform='yj', player_id=None, roster=None, summary=False):
-    if player_id and type(roster) != None: # TODO: add logic for if the player ID is given but not a roster (use function in package)
-        player_name = roster[roster['Unnamed: 0'] == player_id]
-    player_df = data[data['name'] == player_name]
-    player_df.drop_duplicates(subset='date', keep='first', inplace=True)
-    player_train_df = player_df[player_df['date'] < forecast_from]
-    player_test_df = player_df[player_df['date'] >= forecast_from]
-    player_train_df = player_train_df.loc[:, [index, feature]]
-    player_train_df = player_train_df.set_index(index, drop=True)
-    if player_train_df.shape[0] == 0:
-        st.write('{} is a rookie!'.format(player_name))
-        return None
-    if transform == 'log':
-        player_train_df.loc[:, 'logValues'] = np.log(player_train_df['cumStatpoints']) # TODO: make this stat agnostic
-    elif transform == 'yj':
-        transformer = PowerTransformer()
-        transformer.fit(player_train_df.values.reshape(-1, 1))
-        player_train_df.loc[:, 'transformedValues'] = transformer.transform(player_train_df['cumStatpoints'].values.reshape(-1, 1))
-        player_train_df.drop('cumStatpoints', axis=1, inplace=True)
-    player_test_df = player_test_df.loc[:, [index, feature]]
-    player_test_df = player_test_df.set_index(index, drop=True)
-    # player_train_df = player_train_df[:'2018-10-03']
-    # player_test_df = player_test_df['2018-10-03':]
-    if player_test_df.shape[0] == 0:
-        st.write('{} retired!'.format(player_name))
-        return None
-    start_time = time.time()
-    st.write('Searching ARIMA parameters for {}...'.format(player_name))
-    try:
-        model = pm.auto_arima(player_train_df, start_p=1, start_q=1,
-                                max_p=5, max_q=5, max_d=3, m=3,
-                                start_P=0, start_Q=0, seasonal=True,
-                                information_criterion='aicc',
-                                error_action='ignore',  
-                                suppress_warnings=True, 
-                                stepwise=True)
-        st.write('Model built, fitting...')
-        model.fit(player_train_df)
-    except ValueError:
-        st.write("{} doesn't have enough data!".format(player_name))
-        return None
-    except IndexError:
-        st.write('Index error for {}'.format(player_name))
-        return None
-    except:
-        st.write('Unhandled error for {}'.format(player_name))
-        return None
-    predictions, intervals = model.predict(n_periods=player_test_df.shape[0], return_conf_int=True)
-    if transform == 'log':
-        predictions = np.exp(predictions)
-        intervals = np.exp(intervals)
-    elif transform == 'yj':
-        predictions = transformer.inverse_transform(predictions.reshape(-1, 1))
-        low_intervals = transformer.inverse_transform(intervals[:,0].reshape(-1, 1))
-        high_intervals = transformer.inverse_transform(intervals[:,1].reshape(-1, 1))
-    end_time = time.time()
-    if transform != 'yj':
-        low_intervals = []
-        high_intervals = []
-        for low, high in intervals:
-            low_intervals.append(low)
-            high_intervals.append(high)
-    prediction_residuals = calculate_test_residuals(predictions, player_test_df)
-    if summary: st.text(model.summary())
-    train_residuals = pd.DataFrame(model.resid())
-    trainMfe, trainMae, trainRmse, trainResiduals = calculate_errors(train_residuals)
-    testMfe, testMae, testRmse, testResiduals = calculate_errors(prediction_residuals)
-    model_params = model.get_params()
-    p, d, q = model_params['order']
-    try:
-        P, D, Q, m = model_params['seasonal_order']
-    except TypeError:
-        st.write('Search failed to find valid options.')
-        return None
-    st.write("{0}'s Auto-ARIMA({1},{2},{3})({4},{5},{6},{7}) took {8:.3f} seconds.".format(player_name, p, d, q, P, D, Q, m, end_time-start_time))
-    results_df = pd.DataFrame({'forecastStart':forecast_from,
-                                'aic':model.aic(),
-                                'p':p,
-                                'd':d,
-                                'q':q,
-                                'P':P,
-                                'D':D,
-                                'Q':Q,
-                                'm':m,
-                                'trainMfe':trainMfe, # TODO: store more info (eg. confidence intervals, )
-                                'trainMae':trainMae,
-                                'trainRmse':trainRmse,
-                                'trainResiduals':[train_residuals],
-                                'testMfe':testMfe,
-                                'testMae':testMae,
-                                'testRmse':testRmse,
-                                'testResiduals':[prediction_residuals], 
-                                'intervalLow':[low_intervals],
-                                'intervalHigh':[high_intervals]}, index=[player_name])
-    return results_df
-
 # arima_response = player_arima(data, player_name='Leon Draisaitl', transform='yj', summary=True).values.tolist()
 # st.dataframe(arima_response)
 
-# @st.cache
-def all_player_arima(data, roster, transform='none', print_status=False):
-    if print_status: print('Running Auto-ARIMAs...')
-    results = pd.DataFrame()
-    for index, player in roster.iterrows():
-        if print_status: print('Player {}'.format(index))
-        player_name = player['name']
-        player_results = player_arima(data, player_name=player_name, transform=transform)
-        if type(player_results) is type(None):
-            st.write('Skipping {}'.format(player_name))
-            continue
-        st.dataframe(player_results)
-        results = pd.concat([results, player_results])
-        results.to_pickle('./data/temp/arima_results_TEMP.p')
-    if print_status: print('Done!')
-    return results
-
-all_arima_results = nhl.all_player_arima(data, roster=full_roster, transform='yj', print_status=False)
+arima_results = nhl.player_arima(data, player_name='Leon Draisaitl', transform='none')
+st.dataframe(arima_results)
+# all_arima_results = nhl.all_player_arima(data, roster=full_roster, transform='yj', print_status=False)
--- AutoDraft/arima_analysis.py
+++ AutoDraft/arima_analysis.py
@@ -19,16 +19,23 @@ def load_csv(path='./data/full_dataset_4_seasons.csv'):
     data = pd.read_csv(path)
     return data
 
-# @st.cache
-def load_pickle(path='./data/arima_results_m3.p'):
+@st.cache(ignore_hash=True)
+def load_results(path='./data/arima_results_m3.p'):
+    data = pd.read_pickle(path)
+    data.loc[:,'name'] = data.index
+    data.drop_duplicates('name', inplace=True)
+    return data
+
+@st.cache(ignore_hash=True)
+def load_transformed_results(path='./data/arima_results_m3_yj.p'):
     data = pd.read_pickle(path)
     data.loc[:,'name'] = data.index
     data.drop_duplicates('name', inplace=True)
     return data
 
 data = load_csv()
-results = load_pickle()
-results_yj = load_pickle('./data/arima_results_m3_yj.p')
+results = load_results()
+results_yj = load_transformed_results('./data/arima_results_m3_yj.p')
 
 st.text('Stats data shape: {0}\nARIMA results shape: {1}'.format(data.shape, results.shape))
 st.write('Stat dataframe head:')
@@ -38,178 +45,11 @@ st.dataframe(results)
 st.write('Yeo-Johnsoned ARIMA results dataframe:')
 st.dataframe(results_yj)
 
-def get_hists(metric_list=['testRmse'], results=[results, results_yj], result_names=['Raw', 'Yeo-Johnson'], range_result=None, range_metric=None, edges_method='min'):
-    hists = []
-    edges_list = []
-    for result, name in zip(results, result_names):
-        for metric in metric_list:
-            hist, edges = np.histogram(result[metric])
-            hists.append(hist)
-            edges_list.append(edges)
-    edge_min = edges_list[0][-1]
-    edge_max = edges_list[0][-1] # I cheated here
-    edge_loc = None
-    for i, edges in enumerate(edges_list):
-        edge_range = edges[-1] - edges [0]
-        if edges_method == 'max':
-            if edge_range > edge_max: edge_loc = i
-        else:
-            if edge_range < edge_min: edge_loc = i
-    if not edge_loc: edge_loc = 0
-    edges = edges_list[edge_loc]
-    st.dataframe(edges)
-    return hists, edges
-
-def plot_hists(metric_list=['testRmse'], results=[results, results_yj], result_names=['Raw', 'Yeo-Johnson']):
-    fig = figure(plot_height = 600, plot_width = 600, 
-                    title = "Histogram of ARIMA RMSE's",
-                    x_axis_label = 'RMSE', 
-                    y_axis_label = '# of players')
-
-    results_df = pd.DataFrame()
-    for result, name in zip(results, result_names):
-        metrics_df = pd.DataFrame()
-        for metric in metric_list:
-            metric_df = result[metric]
-            metrics_df = pd.concat([metrics_df, metric_df], axis=1)
-        metrics_df.columns = [column + name for column in metrics_df.columns]
-        results_df = pd.concat([results_df, metrics_df], axis=1)
-        # result_df = pd.concat([result[metric] for metric in metric_list], axis=1) # TODO: I don't think multiple metrics will play nice here...
-    max_range = results_df.max() - results_df.min()
-    max_range_results = max_range.idxmax()
-    st.dataframe(max_range)
-
-    hists, edges = get_hists(metric_list, results, result_names, edges_method='max') # TODO: add scaling of axes back in
-    # TODO: handle colors/length mismatch
-    for result_name, hist, color in zip(result_names, hists, ['blue', 'red', 'green']):
-        fig.quad(bottom=0, top=hist, 
-                        left=edges[:-1], right=edges[1:], 
-                        fill_color=color, line_color='black', legend=result_name)
-
-    fig.legend.location = 'top_right'
-    fig.legend.click_policy = 'hide'
-
-    st.bokeh_chart(fig)
-    return fig
-
 # plot_hists(['testRmse'])
 
 test_player = st.text_input('Player to predict:', 'Leon Draisaitl')
 
-# @st.cache
-def calculate_predictions(data=data, results=results, player_name=test_player, target='cumStatpoints'):
-    test_results = results.loc[test_player, :]
-    test_residuals = test_results.testResiduals
-    train_residuals = test_results.trainResiduals
-    test_real = data[data['name'] == test_player].loc[:, ['date', target]]
-    # test_real.set_index(pd.to_datetime(test_real['date']), inplace=True)
-
-    # st.dataframe(test_real)
-    # st.write([frame.shape for frame in [test_results, train_residuals, test_residuals]])
-
-    full_residuals = pd.concat([train_residuals, test_residuals], axis=0)
-    full_residuals.reset_index(inplace=True, drop=True)
-    full_residuals.columns = ['residuals']
-    test_real.reset_index(inplace=True, drop=True)
-
-    # st.write(full_residuals.shape)
-    # st.write(test_real.shape)
-    full_frame = pd.concat([test_real, full_residuals], axis=1)
-    full_frame['date'] = pd.to_datetime(full_frame['date'])
-    full_frame.drop_duplicates(subset='date', keep='first', inplace=True)
-    full_frame.set_index('date', drop=False, inplace=True)
-    full_frame['predictions'] = full_frame.apply(lambda row: row.cumStatpoints - row.residuals, axis=1)
-    # st.dataframe(full_frame)
-    return full_frame, player_name
-
-def return_intervals(results=results, player_name=test_player):
-    lows = results.loc[test_player, 'intervalLow']
-    highs = results.loc[test_player, 'intervalHigh']
-    # st.write(type(lows))
-    # for data in [lows, highs]:
-    #     if type(data) == type(np.ndarray((1,1))): data = data.tolist() 
-    # st.write(type(lows))
-    try:
-        intervals = pd.DataFrame({'low':lows, 'high':highs})
-    except ValueError:
-        intervals = pd.DataFrame({'low':lows.tolist(), 'high':highs.tolist()})
-    return intervals
-
 # test_intervals = return_intervals()
-
-# @st.cache
-def plot_actual_predictions_series(results, target='cumStatpoints', metric='Rmse',
-                                    series_dataframe=calculate_predictions(player_name=test_player, target='cumStatpoints')[0],
-                                    player_name=calculate_predictions(player_name=test_player, target='cumStatpoints')[1]):
-    intervals = return_intervals(results, player_name)
-    dates = series_dataframe.index.values.astype(np.datetime64)
-    start_date = dt.strptime('2018-10-03', '%Y-%m-%d')
-
-    real_source = ColumnDataSource(data=dict(date=dates, points=series_dataframe[target]))
-    pred_source = ColumnDataSource(data=dict(date=dates, points=series_dataframe['predictions']))
-    interval_dates = dates[-intervals.shape[0]:].reshape(-1,1)
-    interval_dates = np.hstack((interval_dates, interval_dates))
-    interval_source = ColumnDataSource(data=dict(date=interval_dates, points=intervals))
-
-    player_line = figure(title='{0}({1},{2},{3})({4},{5},{6},{7}) [Train RMSE: {8:.3f}, Test RMSE: {9:.3f}]'.format(test_player, 
-                                                                                                                    results.loc[player_name, 'p'],
-                                                                                                                    results.loc[player_name, 'd'],
-                                                                                                                    results.loc[player_name, 'q'],
-                                                                                                                    results.loc[player_name, 'P'],
-                                                                                                                    results.loc[player_name, 'D'],
-                                                                                                                    results.loc[player_name, 'Q'],
-                                                                                                                    3, # TODO: undo hardcoding once ARIMA results are regenerated
-                                                                                                                    results.loc[player_name, 'train'+metric],
-                                                                                                                    results.loc[player_name, 'test'+metric]), # TODO: change to MASE
-                            plot_height=300, plot_width=800, tools="xpan", toolbar_location='above',
-                            x_axis_type="datetime", x_axis_location="below", x_range=(dates[0], dates[-1]),
-                            background_fill_color="#efefef")
-
-    hover_tool = HoverTool(tooltips=[("date", "@date"),
-                                    ("points", "@points")],
-                            mode='vline')
-
-    player_line.circle('date', 'points', source=real_source, line_color='blue', legend='actual')
-    player_line.line('date', 'points', source=pred_source, line_color='red', legend='predicted')
-    # player_line.patch(x=dates[-intervals.shape[0]:], y=intervals, line_color='red', alpha=0.4)
-
-    player_line.varea(x=interval_dates[:, 0], y1=intervals.loc[:, 'high'], y2=intervals.loc[:, 'low'], fill_alpha=0.4, color='red', legend='predicted')
-    # interval_glyph = Patch(x='date', y='points', fill_color="red", fill_alpha=0.4)
-    # player_line.add_glyph(interval_source, interval_glyph)
-
-    player_line.legend.location = 'top_left'
-    player_line.legend.click_policy = 'hide'
-    player_line.add_tools(hover_tool)
-    player_line.toolbar.active_multi = hover_tool
-    # player_line.yaxis.axis_label('Cumulative Points')
-
-    test_start = Span(location=start_date,
-                              dimension='height', line_color='green',
-                              line_dash='dashed', line_width=3)
-    player_line.add_layout(test_start)
-
-    select = figure(title="Drag the middle and edges of the selection box to change the range above",
-                    plot_height=130, plot_width=800, y_range=player_line.y_range,
-                    x_axis_type="datetime", y_axis_type=None,
-                    tools="", toolbar_location=None, background_fill_color="#efefef", x_range=(dates[0], dates[-1]))
-
-    range_tool = RangeTool(x_range=player_line.x_range)
-    range_tool.overlay.fill_color = "navy"
-    range_tool.overlay.fill_alpha = 0.2
-
-    select.line('date', 'points', source=real_source, line_color='blue')
-    select.line('date', 'points', source=pred_source, line_color='red')
-    select.ygrid.grid_line_color = None
-    select.add_tools(range_tool)
-    select.toolbar.active_multi = range_tool
-    select.add_layout(test_start)
-
-    chart = column(player_line, select)
-
-    st.bokeh_chart(chart)
-    st.write("{}'s dataframe:".format(player_name))
-    st.dataframe(series_dataframe)
-    return chart
 
 transform = st.checkbox('Use transformed (YJ) data?')
 if not transform:
